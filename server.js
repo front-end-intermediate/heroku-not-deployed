858f61ff339bee658b95c276aec72e2b338eb43a
DanielDeverell <daniel@getperksy.com>
2019-11-25 20:33:56 -0500

    done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+const path = require('path');
 require("dotenv").config();
 
 const recipeModel = require("./api/recipe.model");
@@ -9,7 +10,6 @@ const recipeControllers = require("./api/recipe.controllers");
 const app = express();
 
 app.use(fileUpload());
-app.use(express.static("public"));
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
@@ -23,6 +23,13 @@ app.put("/api/recipes/:id", recipeControllers.update);
 app.delete("/api/recipes/:id", recipeControllers.delete);
 app.get("/api/import", recipeControllers.import);
 app.get("/api/killall", recipeControllers.killall);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 mongoose
   .connect(dataBaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
